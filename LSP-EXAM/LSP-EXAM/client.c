af2b0ebccd79de58b6c32e7bfc9e0bfbf9683400
Armands Baurovskis <baurovskis.armands@gmail.com>
2014-01-17 17:23:23 +0200

    -Pieprasa sakuma lietotaja username un servera ip. Var veikt registrāciju. Pārbauda vai istaba jau ir pilna vai jau ir sācies mačs. Ja visi iziet, tad spēles sākums tiek sākts skaitīts no jauna.

--- LSP-EXAM/LSP-EXAM/client.c
+++ LSP-EXAM/LSP-EXAM/client.c
@@ -11,58 +11,114 @@
 #include<sys/socket.h>
 #include<arpa/inet.h> 
 #include <unistd.h>
-
-int main(int argc , char *argv[])
+#include <ncurses.h>
+#include <stdlib.h>
+typedef struct
 {
-    int sock;
+    char nickname[100];
+    int userId;
+    
+} UserInformation;
+UserInformation user;
+char * mesg;	/* message to be appeared on the screen */
+int row,col;
+int sock;
+
+void createConnection(char server_ip[], char userNickName[])
+{
     struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
+    char message[1000] , server_reply[1000];
     
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
     {
-        printf("Could not create socket");
     }
-    puts("Socket created");
     
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_addr.s_addr = inet_addr(server_ip);
     server.sin_family = AF_INET;
     server.sin_port = htons( 8888 );
     
     //Connect to remote server
     if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
     {
-        perror("connect failed. Error");
-        return 1;
+    
     }
     
-    puts("Connected\n");
+    clear();
+    mesg = "Connected!";
+    mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",mesg);
+    refresh();
     
-    //keep communicating with server
-    while(1)
+    sleep(2);
+    clear();
+    mesg = "Waiting replay from server...";
+    mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",mesg);
+    refresh();
+    //Send new request;
+    sprintf(message, "J %s", user.nickname);
+    send(sock , message , strlen(message) , 0);
+    
+    if( recv(sock , server_reply , 1000 , 0) > 0)
     {
-        printf("Enter message : ");
-        scanf("%s" , message);
+        char *userID;
+        char *start_time;
+        int start_time_int;
+        char *command;
+        char msgToScreen[200];
+        char *serv_message = malloc(sizeof(server_reply));
+        sprintf(serv_message, "%s",server_reply);
         
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
+        command = strtok(server_reply, " ");
+        if(strcmp(command, "J") != 0){
+            clear();
+            mvprintw(row/2,(col-strlen(serv_message))/2.0,"%s",serv_message);
+            refresh();
+            sleep(2);
+            clear();
+            endwin();
+            exit(EXIT_FAILURE);
+        }
+        userID = strtok(NULL, " ");
+        start_time = strtok(NULL, " ");
+        start_time_int = atoi(start_time);
+        user.userId = atoi(userID);
+        while (start_time_int!=0) {
+            clear();
+            sprintf(msgToScreen, "The game will start in %i seconds",start_time_int);
+            mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",msgToScreen);
+            refresh();
+            sleep(1);
+            start_time_int--;
         }
         
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
+        clear();
+        mesg = "Downloading map....";
+        mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",mesg);
         
-        puts("Server reply :");
-        puts(server_reply);
+        refresh();
+        while(1){ refresh(); };
     }
+}
+
+int main(int argc , char *argv[])
+{
+    mesg ="Enter a server IP address: ";		/* message to be appeared on the screen */
+    char serverIP[100];
+    initscr();
+    cbreak();
+    keypad(stdscr, TRUE);/* start the curses mode */
+    getmaxyx(stdscr,row,col);		/* get the number of rows and columns */
+    mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",mesg);
+    /* print the message at the center of the screen */
+    getstr(serverIP);
+    clear();
+    mesg = "Enter your nickname: ";
+    mvprintw(row/2,(col-strlen(mesg))/2.0,"%s",mesg);
+    getstr(user.nickname);
+    //sprintf(serverIP, "127.0.0.1");
+    //sprintf(user.nickname,"ArmandsB");
+    createConnection(serverIP, user.nickname);
     
-    close(sock);
     return 0;
 }
--- LSP-EXAM/LSP-EXAM/server.c
+++ LSP-EXAM/LSP-EXAM/server.c
@@ -15,11 +15,68 @@
 #include<unistd.h>
 #include<pthread.h>
 
+//Definētie lauki
+struct User
+{
+    char nickname[100];
+    int userId;
+    int planetCount;
+    int userSocket;
+    struct User *next;
+    
+} typedef User;
+
+int userIDS = 1;
+int REG_FLAG = 1;
+int playerCount = 0;
+
+int PLAYERS;
+int START_TIME_DEFINE;
+int SPEED;
+int MIN_CAPACITY;
+int MAX_CAPACITY;
+
+int START_TIME;
+pthread_t start_game_thread;
+
+User *root = NULL;
+
 //the thread function
 void *connection_handler(void *);
+//User message process
+void processUserMessage(char usermessage[],void *socket_desc);
+void registerUserForTheGame(char userData[],void *socket_desc);
 
 int main(int argc , char *argv[])
 {
+    
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    fp = fopen("/Users/armandsbaurovskis/Documents/Developer/LSP-EXIS/LSP-EXAM/LSP-EXAM/config.txt", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    
+    while ((read = getline(&line, &len, fp)) != -1) {
+        char * command = strtok(line, " ");
+        char * value = strtok(NULL, " ");
+        if (strcmp(command, "PLAYERS")==0) {
+            PLAYERS = atoi(value);
+        }else if (strcmp(command, "SPEED")==0) {
+            SPEED = atoi(value);
+        }else if (strcmp(command, "START_TIME")==0) {
+            START_TIME_DEFINE = atoi(value);
+        }else if (strcmp(command, "MIN_CAPACITY")==0) {
+            MIN_CAPACITY = atoi(value);
+        }else if (strcmp(command, "MAX_CAPACITY")==0) {
+            MAX_CAPACITY = atoi(value);
+        }
+    }
+    START_TIME = START_TIME_DEFINE;
+
+    
     int socket_desc , client_sock , c , *new_sock;
     struct sockaddr_in server , client;
     
@@ -44,7 +101,6 @@ int main(int argc , char *argv[])
         return 1;
     }
     puts("bind done");
-    
     //Listen
     listen(socket_desc , 3);
     c = sizeof(struct sockaddr_in);
@@ -83,31 +139,42 @@ int main(int argc , char *argv[])
  * This will handle connection for each client
  * */
 
+void *countDownStartTime(void *arg)
+{
+
+    while(START_TIME!=0)
+    {
+        sleep(1);
+        START_TIME--;
+    }
+    return 0;
+}
 
 void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
     long read_size;
-    char *message , client_message[2000];
+    char client_message[1000];
     
-    //Send some messages to the client
-    message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
-    
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));
     
     //Receive a message from client
     while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
     {
-        //Send the message back to client
-        write(sock , client_message , read_size);
+        processUserMessage(client_message, socket_desc);
     }
     
     if(read_size == 0)
     {
         puts("Client disconnected");
+        if(REG_FLAG==1){
+            if(playerCount>0)
+                playerCount--;
+            if(playerCount == 0){
+                pthread_cancel(start_game_thread);
+                START_TIME = START_TIME_DEFINE;
+            }
+        }
         fflush(stdout);
     }
     else if(read_size == -1)
@@ -119,4 +186,65 @@ void *connection_handler(void *socket_desc)
     free(socket_desc);
     
     return 0;
+}
+void processUserMessage(char usermessage[],void *socket_desc)
+{
+    char command = usermessage[0];
+    switch (command) {
+        case 'J':
+            registerUserForTheGame(usermessage,socket_desc);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+void registerUserForTheGame(char userData[],void *socket_desc)
+{
+    int sock = *(int*)socket_desc;
+    char *userNickName;
+    userNickName = strtok(userData, " ");
+    userNickName = strtok(NULL, " ");
+    User * user;
+    user = (User *) malloc( sizeof(User) );
+    strcpy(user->nickname, userNickName);
+    user->userId = userIDS;
+    user->planetCount = 1;
+    user->next = NULL;
+    user->userSocket = sock;
+    if(root == NULL)
+        root=user;
+    else{
+        User*p = root;
+        while (p->next!=NULL) {
+            p = p->next;
+        }
+        p->next = user;
+    }
+    
+    char message[1000];
+    playerCount++;
+    if(START_TIME <=0)
+    {
+        sprintf(message, "Game has already started");
+        send(sock , message , strlen(message) , 0);
+        
+    }
+    else if(playerCount > PLAYERS)
+    {
+        sprintf(message, "The room is full");
+        send(sock , message , strlen(message) , 0);
+
+    }else{
+        sprintf(message, "J %i %i",userIDS,START_TIME);
+        userIDS++;
+        send(sock , message , strlen(message) , 0);
+    }
+    
+    if(playerCount==1)
+    {
+        pthread_create(&start_game_thread, NULL, &countDownStartTime, NULL);
+    }
+    
 }
